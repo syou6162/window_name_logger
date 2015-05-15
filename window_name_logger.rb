daa568d2724cad17739db61497fe5c3fadb55ceb
Yasuhisa Yoshida <syou6162@gmail.com>
2015-05-15 10:15:41 +0900

    sleepしていたら記録しない

--- window_name_logger.rb
+++ window_name_logger.rb
@@ -4,10 +4,15 @@ require 'json'
 
 # http://stackoverflow.com/questions/480866/get-the-title-of-the-current-active-window-document-in-mac-os-x
 
-frontmost = Appscript.app('System Events').application_processes.get.select{ |a| a.frontmost.get }.first
 result = {}
 
 result["date"] = Time.now.to_s
+STDERR.puts Time.now.to_s
+
+sleep_time=`ioreg -c IOHIDSystem | perl -ane 'if (/Idle/) {$idle=(pop @F)/1000000000; print $idle,"\n"; last}'`.to_f
+exit if sleep_time > 300
+
+frontmost = Appscript.app('System Events').application_processes.get.select{ |a| a.frontmost.get }.first
 
 if frontmost
   result["application"] = frontmost.name.get.to_s
